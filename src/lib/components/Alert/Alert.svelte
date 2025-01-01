bea337b133218db1d9c2e59e956af4ee162a3adf
dansilvaUT <dan13silv@gmail.com>
2025-01-01 10:55:15 -0700

    alerts.

---
+++ src/lib/components/Alert/Alert.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+	export let type: 'red' | undefined = 'red';
+	export let message = 'Something went wrong. Try again.';
+</script>
+
+<Alert border color={type} class="my-4 w-full">
+	{message}
+</Alert>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { Label, Input, Heading, GradientButton } from 'flowbite-svelte';
+	import { Input, Heading, GradientButton } from 'flowbite-svelte';
 	import Nav from '$lib/components/Nav/Nav.svelte';
 	import getPokemon from '$lib/api/pokemon/index';
 	import Pokemon from '$lib/components/Pokemon/Pokemon.svelte';
+	import Alert from '$lib/components/Alert/Alert.svelte';
+	import { fade } from 'svelte/transition';
 
 	type PokemonData = {
 		id: number;
@@ -13,6 +15,7 @@
 
 	$: pokemon = '';
 	let pokemonData: PokemonData;
+	$: error = false;
 
 	const handleChange = (e: Event) => {
 		const target = e.target as HTMLInputElement | null;
@@ -22,17 +25,30 @@
 	};
 	const fetchPokemon = async (e: SubmitEvent) => {
 		e.preventDefault();
-		if (!pokemon) return;
+		if (!pokemon) {
+			error = true;
+			setTimeout(() => {
+				error = false;
+			}, 3000);
+			return;
+		}
 		const data = await getPokemon(pokemon);
+		pokemon = '';
 		pokemonData = { id: data.id, name: data.name, images: data.sprites, abilities: data.abilities };
 	};
 </script>
 
 <Nav />
-<section class="mt-20 flex flex-col items-center">
+<section class="mt-28 flex flex-col items-center">
 	<Heading tag="h1" class="text-center text-4xl font-bold	">Pokedex Search</Heading>
+
+	{#if error}
+		<div transition:fade={{ duration: 300 }} class="w-2/4">
+			<Alert type="red" message="Please enter a Pokemon name." />
+		</div>
+	{/if}
 	<form
-		class="mobile-only:flex-col mobile-only:w-full mobile-only:px-3 mb-6 flex w-1/2 justify-between"
+		class="mobile-only:flex-col mobile-only:w-full mobile-only:px-3 mb-6 mt-4 flex w-1/2 justify-between"
 		onsubmit={fetchPokemon}
 	>
 		<Input
